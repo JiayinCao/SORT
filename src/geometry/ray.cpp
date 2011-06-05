864b5686276611935a182aa1b6a0fa45ed2a539f
codeboycjy <codeboycjy@32f55271-c391-4a20-9b51-6f3d7b68033c>
2011-06-05 06:49:09 +0000

    add ray interfaces and a bounding box class

--- src/geometry/ray.cpp
+++ src/geometry/ray.cpp
@@ -12,18 +12,29 @@ Ray::Ray()
 {
 }
 // constructor from a point and a direction
-Ray::Ray( const Point& p , const Vector& dir )
+Ray::Ray( const Point& p , const Vector& dir , unsigned depth , float fmin , float fmax)
 {
 	m_Ori = p;
 	m_Dir = dir;
+	m_Depth = depth;
+	m_fMin = fmin;
+	m_fMax = fmax;
 }
 // copy constructor
 Ray::Ray( const Ray& r )
 {
 	m_Ori = r.m_Ori;
 	m_Dir = r.m_Dir;
+	m_Depth = r.m_Depth;
+	m_fMin = r.m_fMin;
+	m_fMax = r.m_fMax;
 }
 // destructor
 Ray::~Ray()
+{
+}
+
+// operator to get a point on the ray
+Point Ray::operator ()( float t ) const
 {
 }
