e44201bc4a1b295a95219a77f39fc8adc4c07050
codeboycjy <codeboycjy@32f55271-c391-4a20-9b51-6f3d7b68033c>
2011-06-09 16:12:30 +0000

    kill the warning in windows compiling

--- src/sort.h
+++ src/sort.h
@@ -16,6 +16,14 @@
 // use default name space
 using namespace std;
 
+// find the correct platform
+// currently , windows and linux is support
+#if defined(_WIN32) || defined(_WIN64)
+	#define SORT_IN_WINDOWS
+#elif defined(__linux__)
+	#define SORT_IN_LINUX
+#endif
+
 // some useful macro
 #define SAFE_DELETE(p) { if(p) { delete p; p = 0; } }
 #define SAFE_DELETE_ARRAY(p) { if(p) { delete[] p; p = 0 ; } }
--- src/utility/error.h
+++ src/utility/error.h
@@ -14,12 +14,18 @@
 // u could define SORT_DISABLE_ASSERT to disable all of the assert in sort!!!
 // #define	SORT_DISABLE_ASSERT
 
+#if defined( SORT_IN_WINDOWS )
+	#define PRINT_ERROR(s) { char buf[1024]; sprintf_s( buf , 1024 , "Error: %s (File:%s  Line:%d)" , s , __FILE__ , __LINE__ ); LOG(buf); }
+#elif defined( SORT_IN_LINUX )
+	#define PRINT_ERROR(s) { char buf[1024]; sprintf( buf , "Error: %s (File:%s  Line:%d)" , s , __FILE__ , __LINE__ ); LOG(buf); }
+#endif
+
 // here we use macro NOT function to show the file name and line number where the error occurs
 #ifdef SORT_DISABLE_ASSERT
 	#define	SCrash(s)
 	#define SAssert(b)
 #else
-	#define SCrash(s) { char buf[1024]; sprintf( buf , "Error: %s (File:%s  Line:%d)" , s , __FILE__ , __LINE__ ); LOG(buf); abort(); }
+	#define SCrash(s) { PRINT_ERROR(s)  abort(); }
 	#define	SAssert(b) assert(b)
 #endif
 
