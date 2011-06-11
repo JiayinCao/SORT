64797571d640d5be8f1d83a23718bbeb8254c9fa
codeboycjy <codeboycjy@32f55271-c391-4a20-9b51-6f3d7b68033c>
2011-06-11 01:04:13 +0000

    add image texture

---
+++ src/texture/imagetexture.cpp
@@ -0,0 +1,53 @@
+/*
+ * filename :	imagetexture.cpp
+ *
+ * programmer :	Cao Jiayin
+ */
+
+// include the header file
+#include "imagetexture.h"
+#include "../utility/error.h"
+#include "../managers/texmanager/texmanager.h"
+#include "../managers/logmanager.h"
+
+// initialize default data
+void ImageTexture::_init()
+{
+	// set default value
+	m_pMemory = 0;
+}
+
+// get color from image
+const Spectrum& ImageTexture::GetColor( int x , int y ) const
+{
+	// if there is no image, just crash
+	if( 0 == m_pMemory || m_pMemory->m_ImgMem == 0 )
+		SCrash( "There is no memory for the texture!!" );
+
+	// filter the texture coordinate
+	_texCoordFilter( x , y );
+
+	// get the offset
+	int offset = y * m_iTexWidth + x;
+
+	// get the color
+	return m_pMemory->m_ImgMem[ offset ];
+}
+
+// release the texture memory
+void ImageTexture::Release()
+{
+	m_pMemory = 0;
+}
+
+// load image from file
+bool ImageTexture::LoadImage( const std::string& str , TEX_TYPE type )
+{
+	// release an image first
+	Release();
+
+	// get the texture manager
+	TexManager::GetSingleton().Read( str , this , type );
+
+	return true;
+}
---
+++ src/texture/imagetexture.h
@@ -0,0 +1,61 @@
+/*
+ * filename :	imagetexture.h
+ *
+ * programmer :	Cao Jiayin
+ */
+
+#ifndef	SORT_IMAGETEXTURE
+#define	SORT_IMAGETEXTURE
+
+// include the header file
+#include "texture.h"
+
+// pre-declare memory
+struct ImgMemory;
+
+///////////////////////////////////////////////////////////////
+// defination of image texture
+class ImageTexture : public Texture
+{
+// public method
+public:
+	// default constructor
+	ImageTexture() { _init(); }
+	// destructor
+	~ImageTexture() { Release(); }
+
+	// load image from file
+	// para 'str'  : the name of the image file to be loaded
+	// para 'type' : the type of the image , default is bmp
+	// result      : whether loading is successful
+	bool LoadImage( const std::string& str , TEX_TYPE type = TT_BMP );
+
+	// get the texture value
+	// para 'x' :	x coordinate , if out of range , use filter
+	// para 'y' :	y coordinate , if out of range , use filter
+	// result	:	spectrum value in the position
+	virtual const Spectrum& GetColor( int x , int y ) const;
+
+	// it means nothing to set the size of the image file
+	// set the size of the texture
+	virtual void	SetWidth( unsigned w ){}
+	virtual void	SetHeight( unsigned h ){}
+	virtual void	SetSize( unsigned w , unsigned h ){}
+
+	// release the texture memory
+	virtual void Release();
+
+// private field
+private:
+	// array saving the color of image
+	ImgMemory*	m_pMemory;
+
+// private method
+	// initialize default data
+	void	_init();
+
+	// set texture manager as a friend
+	friend class TexManager;
+};
+
+#endif
