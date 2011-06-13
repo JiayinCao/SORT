aefc6f70f1d7a3782f618870e9ae9b3b481f521f
codeboycjy <codeboycjy@32f55271-c391-4a20-9b51-6f3d7b68033c>
2011-06-13 05:46:09 +0000

    add uvtexture and checkboxtexture

---
+++ src/texture/uvtexture.cpp
@@ -0,0 +1,33 @@
+/*
+ * filename :	uvtexture.cpp
+ *
+ * programmer :	Cao Jiayin
+ */
+
+// include header file
+#include "uvtexture.h"
+
+// for outputing color
+static Spectrum color;
+
+// get the texture value
+const Spectrum& UVTexture::GetColor( int x , int y ) const
+{
+	// filter the x y coordinate
+	_texCoordFilter( x , y );
+
+	float u = ((float)x) / m_iTexWidth;
+	float v = ((float)y) / m_iTexHeight;
+
+	//set the color
+	color.SetColor( u , v , 0.0f );
+
+	return color;
+}
+
+// initialize default data
+void UVTexture::_init()
+{
+	m_iTexWidth = 16;
+	m_iTexHeight = 16;
+}
---
+++ src/texture/uvtexture.h
@@ -0,0 +1,37 @@
+/*
+ * filename :	uvtexture.h
+ *
+ * programmer :	Cao Jiayin
+ */
+
+#ifndef	SORT_UVTEXTURE
+#define	SORT_UVTEXTURE
+
+// include the header
+#include "texture.h"
+#include "../spectrum/spectrum.h"
+
+/////////////////////////////////////////////////////////////////////////////
+//	defination of uvtexture
+class	UVTexture : public Texture
+{
+// public function
+public:
+	// constructor
+	UVTexture(){_init();}
+	// destructor
+	~UVTexture(){}
+
+	// get the texture value
+	// para 'x' :	x coordinate , if out of range , use filter
+	// para 'y' :	y coordinate , if out of range , use filter
+	// result	:	spectrum value in the position
+	virtual const Spectrum& GetColor( int x , int y ) const;
+
+//private function
+private:
+	// initialize default data
+	void _init();
+};
+
+#endif
