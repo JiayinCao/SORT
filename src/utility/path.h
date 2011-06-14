ee6dd04ef3a598614ceba75daf0d42f421b7ca08
codeboycjy <codeboycjy@32f55271-c391-4a20-9b51-6f3d7b68033c>
2011-06-14 14:24:14 +0000

    modify path.h in windows

--- src/utility/path.h
+++ src/utility/path.h
@@ -7,6 +7,10 @@
 #ifndef	SORT_PATH
 #define	SORT_PATH
 
+#if defined(SORT_IN_WINDOWS)
+#include <windows.h>
+#endif
+
 // get current directory
 string GetExecutableDir()
 {
@@ -16,7 +20,19 @@ string GetExecutableDir()
 #if defined(SORT_IN_LINUX)
 	int c = readlink( "/proc/self/exe", buf, maxLen - 1 );
 #elif defined(SORT_IN_WINDOWS)
+	// get the current module filename
+	GetModuleFileNameA( NULL , buf , maxLen );
 
+	// remove the file name
+	int len = strlen( buf );
+	for( int i = len - 1 ; i > 0 ; i-- )
+	{
+		if( buf[i] == '\\' )
+		{
+			buf[i+1] = 0;
+			break;
+		}
+	}
 #endif
 
 	return string(buf);
