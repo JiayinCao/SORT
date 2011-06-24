b4ac5484d3e6e7f3c2b966c057d09661ff742bf4
codeboycjy <codeboycjy@32f55271-c391-4a20-9b51-6f3d7b68033c>
2011-06-24 06:03:49 +0000

    add absolute path support ( working directory is not necessary in vs )

--- src/managers/texio/bmpio.cpp
+++ src/managers/texio/bmpio.cpp
@@ -12,6 +12,7 @@
 #include "managers/logmanager.h"
 #include "managers/texmanager.h"
 #include "texture/texture.h"
+#include "utility/path.h"
 
 //-------------------------------------------------
 // define some useful structure
@@ -104,7 +105,7 @@ bool BmpIO::Write( const string& str , const Texture* tex )
 
 	// open the file
 	ofstream file;
-	file.open( str.c_str() , ios::binary );
+	file.open( GetFullPath(str).c_str() , ios::binary );
 
 	// if the file could not be opened , just return an LOG_ERROR
 	if( file.is_open() == false )
@@ -143,7 +144,7 @@ bool BmpIO::Read( const string& str , ImgMemory* mem )
 
 	// open the file
 	ifstream file;
-	file.open( str.c_str() , ios::binary );
+	file.open( GetFullPath(str).c_str() , ios::binary );
 
 	// if the file could not be opened , just return an LOG_ERROR
 	if( file.is_open() == false )
--- src/utility/path.h
+++ src/utility/path.h
@@ -7,35 +7,13 @@
 #ifndef	SORT_PATH
 #define	SORT_PATH
 
-#if defined(SORT_IN_WINDOWS)
-#include <windows.h>
-#endif
+// include header file
+#include "sort.h"
 
 // get current directory
-string GetExecutableDir()
-{
-	const int maxLen = 2048;
-	char buf[ maxLen ];
+string GetExecutableDir();
 
-#if defined(SORT_IN_LINUX)
-	int c = readlink( "/proc/self/exe", buf, maxLen - 1 );
-#elif defined(SORT_IN_WINDOWS)
-	// get the current module filename
-	GetModuleFileNameA( NULL , buf , maxLen );
-
-	// remove the file name
-	int len = strlen( buf );
-	for( int i = len - 1 ; i > 0 ; i-- )
-	{
-		if( buf[i] == '\\' )
-		{
-			buf[i+1] = 0;
-			break;
-		}
-	}
-#endif
-
-	return string(buf);
-}
+// get full path
+string GetFullPath( const string& str );
 
 #endif
