644d5745098eef4e51ff2df78b87e2b3b117ce72
codeboycjy <codeboycjy@32f55271-c391-4a20-9b51-6f3d7b68033c>
2011-07-28 13:38:36 +0000

    generate tangent for triangle with the same texture coordinate

--- src/managers/meshmanager.cpp
+++ src/managers/meshmanager.cpp
@@ -348,7 +348,13 @@ Vector BufferMemory::_genTagentForTri( const Trunk* trunk , unsigned k ) const
 
 	float determinant = du1 * dv2 - dv1 * du2 ;
 	if( determinant == 0.0f )
+	{
 		LOG_WARNING<<"There is a triangle containing three vertexes with same texture coodinate , can't generate shading coordinate correctly."<<ENDL;
+		Vector n = Normalize( p0 - p1 );
+		Vector t0 , t1;
+		CoordinateSystem( n , t0 , t1 );
+		return t0;
+	}
 
 	float invdet = 1.0f / determinant;
 	
