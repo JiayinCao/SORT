6c77823fd50c7d0996c00e017ac6a04c49159087
JiayinCao <caojiayin1985@gmail.com>
2011-08-01 14:47:42 +0000

    add 1d/2d distribution sampling method

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,9 @@
 // include the header file
 #include "sort.h"
 #include "system.h"
+#include "utility/samplemethod.h"
+#include "texture/imagetexture.h"
+#include "texture/rendertarget.h"
 
 // the global system
 System g_System;
@@ -14,6 +17,41 @@ System g_System;
 // the main func
 int main( int argc , char** argv )
 {
+	ImageTexture image;
+	image.SetProperty( "filename" , "../res/test.bmp" );
+	
+	Distribution2D dis( &image );
+	unsigned nu = image.GetWidth();
+	unsigned nv = image.GetHeight();
+
+	RenderTarget rt;
+	rt.SetSize( image.GetWidth() , image.GetHeight() );
+
+	srand( time(0) );
+	for( int i = 0 ; i < 1000000; i++ )
+	{
+		float u = ((float) ( rand() % 1024 ) / 1024.0f);
+		float v = ((float) ( rand() % 1024 ) / 1024.0f);
+
+		int t = rand() % 4;
+		while( t-- > 0 )
+			v = (float) (rand() % 1024 ) / 1024.0f;
+
+		float uv[2];
+		dis.SampleContinuous( u , v , uv , 0 );
+
+		int tu = uv[0] * nu;
+		int tv = uv[1] * nv;
+		Spectrum c = rt.GetColor(tu,tv);
+		c+=0.1f;
+
+		rt.SetColor(tu,tv,c);
+	}
+
+	rt.Output( "tt.bmp" );
+
+	return 0;
+
 	// load the scene from file
 	g_System.LoadScene( "../res/first_scene.scene" );
 
@@ -28,4 +66,6 @@ int main( int argc , char** argv )
 
 	// output image
 	g_System.OutputRT( "t.bmp" );
+
+	return 0;
 }
--- src/utility/samplemethod.h
+++ src/utility/samplemethod.h
@@ -7,7 +7,10 @@
 #ifndef	SORT_SAMPLEMETHOD
 #define	SORT_SAMPLEMETHOD
 
+#include <algorithm>
 #include "bsdf/bsdf.h"
+#include "managers/logmanager.h"
+#include "texture/texture.h"
 
 /*
 description :
@@ -96,4 +99,147 @@ inline float UniformSpherePdf( const Vector& v )
 	return INV_TWOPI * 0.5f;
 }
 
+// one dimensional distribution
+class Distribution1D
+{
+// public method
+public:
+	// constructor
+	Distribution1D( const float* f , unsigned n ):
+		count(n)
+	{
+		cdf = 0;
+		sum = 0.0f;
+		if( f == 0 || n == 0 )
+			return;
+
+		cdf = new float[n+1];
+		cdf[0] = 0;
+		for( unsigned i = 0 ; i < n ; i++ )
+			cdf[i+1] = cdf[i] + f[i];
+		sum = cdf[n];
+		for( unsigned i = 0 ; i < n+1 ; i++ )
+			cdf[i] /= sum;
+	}
+	// destructor
+	~Distribution1D(){ SAFE_DELETE_ARRAY(cdf); }
+
+	// get a discrete sample
+	// para 'u' : a canonical random variable
+	// para 'pdf' : propery density function value for the sample
+	// result   : corresponding bucket straddle the u, -1 if there is no data in the distribution
+	int SampleDiscrete( float u , float* pdf ) const
+	{
+		if( count == 0 || cdf == 0 )
+			return -1;
+		if( u >= 1.0f || u < 0.0f )
+			LOG_ERROR<<"Invalid canonical random variable in Distribution1D sample method"<<CRASH;
+
+		float* target = lower_bound( cdf , cdf + count + 1 , u );
+		int offset = (u<=0.0f)? 0:(int)(target-cdf-1);
+		if( pdf ) 
+			*pdf = cdf[offset+1] - cdf[offset];
+		return offset;
+	}
+
+	// get a contineous sample
+	// para 'u' : a canonical random variable
+	// para 'pdf' : property density function value for the sample
+	float SampleContinuous( float u , float* pdf ) const
+	{
+		if( count == 0 || cdf == 0 )
+			return -1.0f;
+		if( u >= 1.0f || u < 0.0f )
+			LOG_ERROR<<"Invalid canonical random variable in Distribution1D sample method"<<CRASH;
+
+		float* target = lower_bound( cdf , cdf+count+1 , u );
+		int offset = (u<=0.0f)?0:(int)(target-cdf-1);
+		if( pdf )
+			*pdf = (cdf[offset+1]-cdf[offset])*count;
+		float du = ( u - cdf[offset] ) / ( cdf[offset+1] - cdf[offset] );
+		return ( du + (float)offset ) / (float)count;
+	}
+
+	// get the sum of the original data
+	float GetSum() const
+	{ return sum; }
+
+	// get the count
+	unsigned GetCount() const
+	{ return count; }
+
+// private field
+private:
+	const unsigned	count;
+	float*	cdf;
+	float	sum;
+};
+
+// two dimensional distribution
+class Distribution2D
+{
+// public method
+public:
+	// default constructor
+	Distribution2D( const float* data , unsigned nu , unsigned nv )
+	{_init( data , nu , nv );}
+	Distribution2D( const Texture* tex )
+	{
+		unsigned nu = tex->GetWidth();
+		unsigned nv = tex->GetHeight();
+		if( nu == 0 || nv == 0 )
+			LOG_ERROR<<"One of the dimensions for the texture is zero!"<<CRASH;
+		float* data = new float[nu*nv];
+		for( unsigned i = 0 ; i < nv ; i++ )
+		{
+			unsigned offset = i * nu;
+			for( unsigned j = 0 ; j < nu ; j++ )
+				data[offset+j] = tex->GetColor( j , i ).GetIntensity();
+		}
+		_init( data , nu , nv );
+		SAFE_DELETE_ARRAY(data);
+	}
+	// destructor
+	~Distribution2D()
+	{
+		SAFE_DELETE( marginal );
+		for( unsigned i = 0 ; i < m_nv ; i++ )
+			SAFE_DELETE(pConditions[i]);
+		pConditions.clear();
+	}
+
+	// get a sample point
+	void SampleContinuous( float u , float v , float uv[2] , float* pdf )
+	{
+		float pdf0 , pdf1;
+		uv[1] = marginal->SampleContinuous( v , &pdf1 );
+		int vi = clamp( (int)(uv[1]*m_nv) , 0 , m_nv - 1 );
+		uv[0] = pConditions[vi]->SampleContinuous( u , &pdf0 );
+
+		if( pdf )
+			*pdf = pdf0 * pdf1;
+	}
+
+// private field
+private:
+	// the distribution in each row
+	vector<Distribution1D*>	pConditions;
+	// the marginal sampleing distribution
+	Distribution1D*			marginal;
+	// the size for the two dimensions
+	unsigned m_nu , m_nv;
+
+	// initialize data
+	void _init( const float* data , unsigned nu , unsigned nv )
+	{
+		for( unsigned i = 0 ; i < nv ; i++ )
+			pConditions.push_back( new Distribution1D( &data[i*nu] , nu ) );
+		float* m = new float[nv];
+		for( unsigned i = 0 ; i < nv ; i++ )
+			m[i] = pConditions[i]->GetSum();
+		marginal = new Distribution1D( m , nv );
+		m_nu = nu;
+		m_nv = nv;
+	}
+};
 #endif
