2b9e468327664f231b54dd9c74e2a943b5a0b4eb
JiayinCao <caojiayin1985@gmail.com>
2011-08-04 05:07:13 +0000

    add comment header in each File Note , the creation time is wrong for the files.

--- src/geometry/transform.h
+++ src/geometry/transform.h
@@ -1,8 +1,19 @@
 /*
- * filename :	transform.h
- *
- * programmer :	Cao Jiayin
- */
+   FileName:      transform.h
+
+   Created Time:  2011-08-04 12:51:01
+
+   Auther:        Cao Jiayin
+
+   Email:         soraytrace@hotmail.com
+
+   Location:      China, Shanghai
+
+   Description:   SORT is short for Simple Open-source Ray Tracing. Anyone could checkout the source code from
+                'sourceforge', https://soraytrace.svn.sourceforge.net/svnroot/soraytrace. And anyone is free to
+                modify or publish the source code. It's cross platform. You could compile the source code in 
+                linux and windows , g++ or visual studio 2008 is required.
+*/
 
 #ifndef	SORT_TRANSFORM
 #define	SORT_TRANSFORM
--- src/texture/uvtexture.h
+++ src/texture/uvtexture.h
@@ -1,8 +1,19 @@
 /*
- * filename :	uvtexture.h
- *
- * programmer :	Cao Jiayin
- */
+   FileName:      uvtexture.h
+
+   Created Time:  2011-08-04 12:45:50
+
+   Auther:        Cao Jiayin
+
+   Email:         soraytrace@hotmail.com
+
+   Location:      China, Shanghai
+
+   Description:   SORT is short for Simple Open-source Ray Tracing. Anyone could checkout the source code from
+                'sourceforge', https://soraytrace.svn.sourceforge.net/svnroot/soraytrace. And anyone is free to
+                modify or publish the source code. It's cross platform. You could compile the source code in 
+                linux and windows , g++ or visual studio 2008 is required.
+*/
 
 #ifndef	SORT_UVTEXTURE
 #define	SORT_UVTEXTURE
