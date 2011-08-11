11619c26e5eccaa1d42fead991f993c0f0147711
JiayinCao <caojiayin1985@gmail.com>
2011-08-11 11:57:54 +0000

    add regular sampling

---
+++ src/sampler/regular.h
@@ -0,0 +1,53 @@
+/*
+   FileName:      regular.h
+
+   Created Time:  2011-08-11 19:40:10
+
+   Auther:        Cao Jiayin
+
+   Email:         soraytrace@hotmail.com
+
+   Location:      China, Shanghai
+
+   Description:   SORT is short for Simple Open-source Ray Tracing. Anyone could checkout the source code from
+                'sourceforge', https://soraytrace.svn.sourceforge.net/svnroot/soraytrace. And anyone is free to
+                modify or publish the source code. It's cross platform. You could compile the source code in 
+                linux and windows , g++ or visual studio 2008 is required.
+ */
+
+#ifndef	SORT_REGULAR
+#define	SORT_REGULAR
+
+#include "sampler.h"
+
+///////////////////////////////////////////////////////////////////////////
+// definition of regular sampling
+class RegularSampler : public Sampler
+{
+// public method
+public:
+	// default constructor
+	RegularSampler(){}
+	// destructor
+	~RegularSampler(){}
+
+	// round the size for sampler
+	// para 'size' : number of samples to be generated
+	// result      : the modified number
+	// note : samples like jittered sampling needs to overwrite the method
+	virtual unsigned RoundSize( unsigned size ) const;
+
+// protected medthod
+protected:
+	// generate sample in one dimension
+	// para 'sample' : the memory to save the sampled data
+	// para 'num'    : the number of samples to be generated
+	virtual void Generate1D( float* sample , unsigned num );
+
+	// generate sample in two dimension
+	// para 'sample' : the memory to save the sampled data
+	// para 'num'    : the number of samples to be generated
+	virtual void Generate2D( float* sample , unsigned num );
+};
+
+#endif
--- src/system.cpp
+++ src/system.cpp
@@ -34,6 +34,7 @@
 
 #include "sampler/stratified.h"
 #include "sampler/random.h"
+#include "sampler/regular.h"
 
 // constructor
 System::System()
@@ -69,20 +70,21 @@ void System::_preInit()
 	m_rt->SetSize( 800 , 600 );
 	// there is default value for camera
 	float distance = 5000.0f;
-	DofPerspective* camera = new DofPerspective();
+//	DofPerspective* camera = new DofPerspective();
+	PerspectiveCamera* camera = new PerspectiveCamera();
 	camera->SetEye( Point( distance , distance * 0.3f , distance ) );
 	camera->SetUp( Vector( 0 , 1 , 0 ) );
 	camera->SetTarget( Point( 0 , distance * 0.05f , 0 ) );
 	camera->SetFov( 3.1415f / 4 );
 	camera->SetRenderTarget( m_rt );
 	Vector vec( distance , distance * 0.25f , distance );
-	camera->SetFocalDistance( vec.Length() );
-	camera->SetLen( 40.0f );
+//	camera->SetFocalDistance( vec.Length() );
+//	camera->SetLen( 40.0f );
 	m_camera = camera;
 	// the integrator
 	m_pIntegrator = new WhittedRT();
 	// the sampler
-	m_pSampler = new StratifiedSampler();
+	m_pSampler = new RegularSampler();
 	m_iSamplePerPixel = m_pSampler->RoundSize(8);
 	m_pSamples = new Sample[m_iSamplePerPixel];
 
