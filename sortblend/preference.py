9285f22aa7946cab9af496abc4cd3d143416ed27
JerryCao1985 <jerrycao_1985@icloud.com>
2015-08-20 22:24:38 +0800

    Create basic sort file

--- sortblend/preference.py
+++ sortblend/preference.py
@@ -1,4 +1,5 @@
 import bpy
+from extensions_framework import util as efutil
 from bpy.types import AddonPreferences
 from bpy.props import StringProperty
 
@@ -9,12 +10,16 @@ class SORTAddonPreferences(AddonPreferences):
             name="Path to SORT binary",
             description='Path to SORT binary',
             subtype='DIR_PATH',
-            default="",
+            default='',
             )
 
     def draw(self, context):
         layout = self.layout
         layout.prop(self, "install_path")
+
+def get_sort_path():
+    addon_prefs = bpy.context.user_preferences.addons[__package__].preferences
+    return efutil.filesystem_path(addon_prefs.install_path) + "/"
 
 def register():
     bpy.utils.register_class(SORTAddonPreferences)
