ed386cbdd86d7085a8739969dc623c7b7b1dbfe1
JerryCao1985 <jerrycao_1985@icloud.com>
2015-08-25 21:46:36 +0800

    expose accelerator in blender

--- sortblend/common.py
+++ sortblend/common.py
@@ -1,7 +1,7 @@
 renderer_bl_name = 'sort_renderer'
 preference_bl_name = __package__
 
-integrator_panel_bl_name = 'IntegratorPanel'
+integrator_panel_bl_name = 'Renderer'
 sampler_panel_bl_name = 'Sampler'
 thread_panel_bl_name = 'MultiThread'
 debug_panel_bl_name = 'DebugPanel'
