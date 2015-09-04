e5d2aaec0dca0244fd50830d82b0d41227528b16
JerryCao1985 <jerrycao_1985@icloud.com>
2015-09-04 22:29:37 +0800

    Add more nodes in blender
    Parser of those nodes is not implemented yet.
    To be done in the near future.

---
+++ src/material/material_node.cpp
@@ -0,0 +1,19 @@
+/*
+   FileName:      material_node.cpp
+
+   Created Time:  2015-09-04 20:22:00
+
+   Auther:        Cao Jiayin
+
+   Email:         soraytrace@hotmail.com
+
+   Location:      China, Shanghai
+
+   Description:   SORT is short for Simple Open-source Ray Tracing. Anyone could checkout the source code from
+                'sourceforge', https://soraytrace.svn.sourceforge.net/svnroot/soraytrace. And anyone is free to
+                modify or publish the source code. It's cross platform. You could compile the source code in 
+                linux and windows , g++ or visual studio 2008 is required.
+*/
+
+#include "material_node.h"
+
