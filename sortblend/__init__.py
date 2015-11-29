3999cad25632f7fd9a1fb775fb1c0797ffeea260
JiayinCao <caojiayin1985@gmail.com>
2015-11-29 10:37:09 +0800

    Add UI support for sky light
    Need to solve the incompatible issue in coordinate translation from
    Blender to SORT

--- sortblend/__init__.py
+++ sortblend/__init__.py
@@ -2,8 +2,8 @@ import bpy
 from . import renderer
 from . import preference
 from . import nodes
-from .ui import ui_init
 from . import material
+from extensions_framework import Addon
 
 bl_info = {
     "name": "SORT",
@@ -15,16 +15,28 @@ bl_info = {
     "warning": "Still under development", # used for warning icon and text in addons panel
     "category": "Render"}
 
+SORTAddon = Addon(bl_info)
+addon_register, addon_unregister = SORTAddon.init_functions()
+
+from .ui import ui_init
+
 def register():
+    addon_register()
+
     bpy.utils.register_module(__name__)
 
     ui_init.register()
     material.register()
     nodes.register()
 
+
+
 def unregister():
+    addon_unregister()
+
     bpy.utils.unregister_module(__name__)
 
     ui_init.unregister()
     material.unregister()
     nodes.unregister()
+
