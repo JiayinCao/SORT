daf0688aed619f4a8610f45b5376f73d76e3acfd
JiayinCao <caojiayin1985@gmail.com>
2015-12-08 21:49:46 +0800

    add tga support

---
+++ src/managers/texio/tgaio.cpp
@@ -0,0 +1,88 @@
+/*
+   FileName:      tgaio.cpp
+
+   Created Time:  2015-12-8
+
+   Auther:        Cao Jiayin
+
+   Email:         soraytrace@hotmail.com
+
+   Location:      China, Shanghai
+
+   Description:   SORT is short for Simple Open-source Ray Tracing. Anyone could checkout the source code from
+                'sourceforge', https://soraytrace.svn.sourceforge.net/svnroot/soraytrace. And anyone is free to
+                modify or publish the source code. It's cross platform. You could compile the source code in 
+                linux and windows , g++ or visual studio 2008 is required.
+*/
+
+// include the header
+#include "tgaio.h"
+#include "texture/texture.h"
+#include "managers/texmanager.h"
+#include "thirdparty/tga/targa.h"
+
+// read data from file
+bool TgaIO::Read( const string& name , ImgMemory* mem )
+{
+	tga_image img;
+    tga_result result;
+    if ((result = tga_read(&img, name.c_str())) != TGA_NOERR) {
+        return false;
+    }
+
+    if (tga_is_right_to_left(&img))
+        tga_flip_horiz(&img);
+    if (!tga_is_top_to_bottom(&img))
+        tga_flip_vert(&img);
+    if (tga_is_colormapped(&img))
+        tga_color_unmap(&img);
+
+	mem->m_iWidth  = img.width;
+	mem->m_iHeight = img.height;
+
+    // "Unpack" the pixels (origin in the lower left corner).
+    // TGA pixels are in BGRA format.
+    mem->m_ImgMem = new Spectrum[ img.width * img.height ];
+  /*  for (int y = img.height - 1; y >= 0; y--)
+        for (int x = 0; x < img.width; x++) {
+            uint8_t *src = tga_find_pixel(&img, x, y);
+            if (tga_is_mono(&img)){
+				mem->m_ImgMem[i] = Spectrum( *src / 255.f );
+			}
+            else {
+                float c[3];
+                c[2] = src[0] / 255.f;
+                c[1] = src[1] / 255.f;
+                c[0] = src[2] / 255.f;
+                mem->m_ImgMem[i] = Spectrum( c );
+            }
+    }*/
+
+	for( int i = 0 ; i < img.height ; i++ )
+		for( int j = 0 ; j < img.width ; j++ )
+		{
+			uint8_t *src = tga_find_pixel(&img, j, i);
+			if (tga_is_mono(&img)){
+				mem->m_ImgMem[i * img.width + j] = Spectrum( *src / 255.f );
+			}
+            else {
+                float c[3];
+                c[2] = src[0] / 255.f;
+                c[1] = src[1] / 255.f;
+                c[0] = src[2] / 255.f;
+                mem->m_ImgMem[i * img.width + j] = Spectrum( c[0] , c[1], c[2] );
+            }
+		}
+
+    tga_free_buffers(&img);
+
+	return true;
+}
+
+// output the texture into bmp file
+bool TgaIO::Write( const string& name , const Texture* tex )
+{
+	
+
+	return true;
+}
---
+++ src/managers/texio/tgaio.h
@@ -0,0 +1,48 @@
+/*
+   FileName:      tgaio.h
+
+   Created Time:  2015-12-8
+
+   Auther:        Cao Jiayin
+
+   Email:         soraytrace@hotmail.com
+
+   Location:      China, Shanghai
+
+   Description:   SORT is short for Simple Open-source Ray Tracing. Anyone could checkout the source code from
+                'sourceforge', https://soraytrace.svn.sourceforge.net/svnroot/soraytrace. And anyone is free to
+                modify or publish the source code. It's cross platform. You could compile the source code in 
+                linux and windows , g++ or visual studio 2008 is required.
+*/
+
+#ifndef	SORT_TGAIO
+#define	SORT_TGAIO
+
+// include the header file
+#include "texio.h"
+
+////////////////////////////////////////////////////////////////////////////
+// definition of bmpio
+// save the image as an bmp file into the file system
+// load the bmp file from file system into texture
+class TgaIO : public TexIO
+{
+// public method
+public:
+	// default constructor
+	TgaIO(){m_TexType=TT_TGA;}
+	
+	// output the texture into bmp file
+	// para 'str' : the name of the outputed bmp file
+	// para 'tex' :	the texture for outputing
+	// result     : 'true' if saving is successful
+	virtual bool Write( const string& str , const Texture* tex );
+
+	// read data from file
+	// para 'str' : the name of the input entity
+	// para 'mem' : the memory for the image
+	// result     :	'true' if the input file is parsed successfully
+	virtual bool Read( const string& str , ImgMemory* mem );
+};
+
+#endif
