be609b300ddb57fbcd7875c463f9535433c84a5b
JiayinCao <caojiayin1985@gmail.com>
2016-01-17 20:04:03 +0800

    minor update

--- src/integrator/integratormethod.cpp
+++ src/integrator/integratormethod.cpp
@@ -77,7 +77,7 @@ Spectrum	EvaluateDirect( const Ray& r , const Scene& scene , const Light* light 
 				return radiance;
 
 			float dot = SatDot( wi , ip.normal );
-			visibility.ray = Ray( ip.intersect , wi , 0 , 0.1f , _ip.t - 0.1f );
+			visibility.ray = Ray( ip.intersect , wi , 0 , 0.001f , _ip.t - 0.001f );
 			if( dot > 0.0f && !li.IsBlack() && visibility.IsVisible() )
 				radiance += li * f * dot * weight / bsdf_pdf;
 		}
