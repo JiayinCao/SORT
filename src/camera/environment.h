ad8a668ae776436712b56c7bbd293890d134e24b
JiayinCao <caojiayin1985@gmail.com>
2016-10-14 19:08:14 +0800

    Add copyright notice

--- src/camera/environment.h
+++ src/camera/environment.h
@@ -1,19 +1,19 @@
 /*
-   FileName:      environment.h
-
-   Created Time:  2011-08-04 12:52:04
-
-   Auther:        Cao Jiayin
-
-   Email:         soraytrace@hotmail.com
-
-   Location:      China, Shanghai
-
-   Description:   SORT is short for Simple Open-source Ray Tracing. Anyone could checkout the source code from
-                'sourceforge', https://soraytrace.svn.sourceforge.net/svnroot/soraytrace. And anyone is free to
-                modify or publish the source code. It's cross platform. You could compile the source code in 
-                linux and windows , g++ or visual studio 2008 is required.
-*/
+    This file is a part of SORT(Simple Open Ray Tracing), an open-source cross
+    platform physically based renderer.
+ 
+    Copyright (c) 2011-2016 by Cao Jiayin - All rights reserved.
+ 
+    SORT is a free software written for educational purpose. Anyone can distribute
+    or modify it under the the terms of the GNU General Public License Version 3 as
+    published by the Free Software Foundation. However, there is NO warranty that
+    all components are functional in a perfect manner. Without even the implied
+    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+    General Public License for more details.
+ 
+    You should have received a copy of the GNU General Public License along with
+    this program. If not, see <http://www.gnu.org/licenses/gpl-3.0.html>.
+ */
 
 #ifndef	SORT_ENVIRONMENT
 #define	SORT_ENVIRONMENT
