9cd8722e200320840a69b05aa35da77c319ab3fe
JiayinCao <caojiayin1985@gmail.com>
2018-03-16 21:56:43 +0800

    Minor update
    - Working on pbrt exporter.

--- sortblend/exporter/pbrt_exporter.py
+++ sortblend/exporter/pbrt_exporter.py
@@ -1,18 +1,24 @@
 import bpy
-import os
 import math
 from . import exporter_common
 from .. import common
 from .. import utility
 
+# utility function to pbrt exporting
+def blender_to_pbrt( vec ):
+    return (vec[0],vec[2],vec[1])
+
 # export blender information
 def export_blender(scene, force_debug=False):
+    export_scene(scene)
+    export_pbrt_file(scene)
+
+# export pbrt file
+def export_pbrt_file(scene):
     # Get the path to save pbrt scene
     addon_prefs = bpy.context.user_preferences.addons[common.preference_bl_name].preferences
     pbrt_file_path = addon_prefs.pbrt_export_path
-    pbrt_file_name = os.path.splitext(os.path.basename(bpy.data.filepath))[0]
-    if not pbrt_file_name:
-        pbrt_file_name = 'default_scene'
+    pbrt_file_name = exporter_common.getEditedFileName()
     pbrt_file_fullpath = pbrt_file_path + pbrt_file_name + ".pbrt"
 
     print( 'Exporting PBRT Scene :' , pbrt_file_fullpath )
@@ -29,9 +35,9 @@ def export_blender(scene, force_debug=False):
     fov = math.degrees( bpy.data.cameras[0].angle )
     camera = exporter_common.getCamera(scene)
     pos, target, up = exporter_common.lookAt(camera)
-    pbrt_camera = "LookAt \t" + utility.vec3tostr( pos ) + "\n"
-    pbrt_camera += "       \t" + utility.vec3tostr( target ) + "\n"
-    pbrt_camera += "       \t" + utility.vec3tostr( up ) + "\n"
+    pbrt_camera = "LookAt \t" + utility.vec3tostr( blender_to_pbrt(pos) ) + "\n"
+    pbrt_camera += "       \t" + utility.vec3tostr( blender_to_pbrt(target) ) + "\n"
+    pbrt_camera += "       \t" + utility.vec3tostr( blender_to_pbrt(up) ) + "\n"
     pbrt_camera += "Camera \t\"perspective\"\n"
     pbrt_camera += "       \t\"float fov\" [" + '%f'%fov + "]\n\n"
 
@@ -40,11 +46,21 @@ def export_blender(scene, force_debug=False):
     pbrt_sampler = "Sampler \"random\" \"integer pixelsamples\" " + '%d'%sample_count + "\n"
 
     # integrator
-    pbrt_integrator = "Integrator \"path\"" + " \"integer maxdepth\" " + '%d'%scene.inte_max_recur_depth + "\n"
+    pbrt_integrator = "Integrator \"path\"" + " \"integer maxdepth\" " + '%d'%scene.inte_max_recur_depth + "\n\n"
 
     file = open(pbrt_file_fullpath,'w')
     file.write( pbrt_film )
     file.write( pbrt_camera )
     file.write( pbrt_sampler )
     file.write( pbrt_integrator )
+    file.write( "WorldBegin\n" )
+    file.write( "Include \"tmp.pbrt\"\n")
+    file.write( "WorldEnd\n" )
     file.close()
+
+# export scene
+def export_scene(scene):
+    all_nodes = exporter_common.renderable_objects(scene)
+    for node in all_nodes:
+        if node.type == 'MESH':
+            print( node.name )
