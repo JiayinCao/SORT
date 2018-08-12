7f69cc079de381c8362eb4fe5b6e130fb9d0cbfd
JerryCao1985 <caojiayin1985@icloud.com>
2018-08-12 19:30:50 +0800

    Show error in blender node link

--- sortblend/exporter/sort_exporter.py
+++ sortblend/exporter/sort_exporter.py
@@ -457,7 +457,7 @@ def export_material(scene, root, force_debug):
                     def socket_node_input(nt, socket):
                         return next((l.from_node for l in nt.links if l.to_socket == socket), None)
                     input_node = socket_node_input(ntree, socket)
-                    sub_xml_node = ET.SubElement( xml_node , 'Property' , name=socket.name , type='node', node=input_node.bl_idname)
+                    sub_xml_node = ET.SubElement( xml_node , 'Property' , name=socket.name , type=socket.export_sort_socket_type(), node=input_node.bl_idname)
 
                     export_props(input_node,sub_xml_node)
                 else:
