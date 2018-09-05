c2d3a5e3b15279be6d964d659dd27f72883ea60f
JiayinCao <caojiayin1985@gmail.com>
2018-09-05 14:07:12 +0800

    Update coat
    - Handle precision issue, which leads to super bright pixels
    - Use refracted direction as input for underlying layer
    - Handle TIR
    - Limit the BRDF so that there is no support for BTDF as underlying layer. It is not supported based on the math.

--- src/bsdf/coat.cpp
+++ src/bsdf/coat.cpp
@@ -20,8 +20,15 @@
 #include "bsdf.h"
 #include "sampler/sample.h"
 
+// Because this BRDF doesn't take TIR ( Total inner reflection ) into account, there is a small biased compensation introduced
+// when evaluating the attenuation upward. The exact number is not mentioned in the original paper, 0.2 is used as default here.
+#define TIR_COMPENSATION    0.2f
+
 Spectrum Coat::F( const Vector& wo , const Vector& wi ) const
 {
+    if (!SameHemiSphere(wo, wi)) return 0.0f;
+    if (!PointingUp(wo)) return 0.0f;
+
     Vector swo = bsdfToBxdf( wo );
     Vector swi = bsdfToBxdf( wi );
     
@@ -34,11 +41,9 @@ Spectrum Coat::F( const Vector& wo , const Vector& wi ) const
         const Spectrum attenuation = ( -thickness * sigma * (1.0f / AbsCosTheta(r_wo) + 1.0f / AbsCosTheta(r_wi))).Exp();
         // Fresnel attenuation between the boundary across layer0 and layer1
         const Spectrum T12 = (1.0f - fresnel.Evaluate(CosTheta(swo)));
-        const Spectrum T21 = (1.0f - fresnel.Evaluate(CosTheta(swi)));
-        // Attenuation of rays coming upward is exactly the same with the downward attenuation, but there is a constant factor for
-        // compensating the loss caused by TIR. The constant number is not mentioned in the original paper, 0.2 is used here by default.
-        static const float G = 0.2f;
-        ret += bottom->f( wo , wi ) * attenuation * T12 * lerp( T21 , 1.0f , G ) / ( ior * ior );
+        const Spectrum T21 = lerp( 1.0f - fresnel.Evaluate(CosTheta(swi)), 1.0f, TIR_COMPENSATION);
+
+        ret += bottom->f( -r_wo , -r_wi ) * attenuation * T12 * T21 / ( ior * ior );
     }
 
     return ret;
@@ -46,47 +51,85 @@ Spectrum Coat::F( const Vector& wo , const Vector& wi ) const
 
 Spectrum Coat::Sample_F( const Vector& wo , Vector& wi , const BsdfSample& bs , float* pPdf ) const{
     Vector swo = bsdfToBxdf( wo );
-    Vector lwi;
+    Vector swi;
     
-    bool tir_o = false;
+    bool tir_o = false , tir_i = false;
     Vector r_wo = refract(swo, Vector(0.0f, 1.0f, 0.0f), ior, 1.0f, tir_o);
+    Vector r_wi;
     const Spectrum attenuation = ( -thickness * sigma * 2.0f / AbsCosTheta(r_wo) ).Exp();
     const float I1 = fresnel.Evaluate(CosTheta(swo)).GetIntensity();
     const float I2 = ( 1.0f - I1 ) * ( 1.0f - I1 ) * attenuation.GetIntensity() / ( ior * ior );
     const float specProp = I1 / ( I1 + I2 );
 
     Spectrum ret;
-    if( bs.u < specProp ){
+    if( bs.u < specProp || specProp == 1.0f ){
         // Importance sampling based on the top layer, Microfacet model
-        ret = coat.sample_f( swo , lwi , BsdfSample(true) , pPdf );
-        wi = bxdfToBsdf(lwi);
+        ret = coat.sample_f( swo , swi, BsdfSample(true) , pPdf );
+        wi = bxdfToBsdf(swi);
+
+        Vector r_wi = refract(swi, Vector(0.0f, 1.0f, 0.0f), ior, 1.0f, tir_i);
+        if (!tir_o && !tir_i) {
+            // Bouguer-Lambert-Beer law
+            const Spectrum attenuation = (-thickness * sigma * (1.0f / AbsCosTheta(r_wo) + 1.0f / AbsCosTheta(r_wi))).Exp();
+            // Fresnel attenuation between the boundary across layer0 and layer1
+            const Spectrum T12 = (1.0f - fresnel.Evaluate(CosTheta(swo)));
+            const Spectrum T21 = lerp(1.0f - fresnel.Evaluate(CosTheta(swi)), 1.0f, TIR_COMPENSATION);
+
+            ret += bottom->f(-r_wo, -r_wi) * attenuation * T12 * T21 / (ior * ior);
+        }
+
+        if(pPdf)
+            *pPdf = lerp(bottom->Pdf(r_wo,r_wi) , *pPdf, specProp);
     }else{
         // Importance sampling using the underlying layer
-        ret = bottom->sample_f( wo , wi , BsdfSample(true) , pPdf );
+        Vector r_wi;
+        ret = bottom->sample_f( -r_wo , r_wi, BsdfSample(true) , pPdf );
+
+        swi = refract(-r_wi, Vector(0.0f, 1.0f, 0.0f), ior, 1.0f, tir_i);
+        wi = bxdfToBsdf(swi);
+
+        // Handle corner case where TIR happens
+        if (tir_i || tir_o) {
+            if (pPdf) *pPdf = 0.0f;
+            return 0.0f;
+        }
+
+        // Bouguer-Lambert-Beer law
+        const Spectrum attenuation = (-thickness * sigma * (1.0f / AbsCosTheta(r_wo) + 1.0f / AbsCosTheta(r_wi))).Exp();
+        // Fresnel attenuation between the boundary across layer0 and layer1
+        const Spectrum T12 = (1.0f - fresnel.Evaluate(CosTheta(swo)));
+        const Spectrum T21 = lerp(1.0f - fresnel.Evaluate(CosTheta(swi)), 1.0f, TIR_COMPENSATION);
+
+        ret *= attenuation * T12 * T21 / (ior * ior);
+
+        if( pPdf )
+            *pPdf = lerp(*pPdf, coat.pdf( swo , swi ) , specProp);
     }
     
-    if( pPdf ){
-        if( bs.u < specProp )
-            *pPdf = lerp( bottom->Pdf( wo , wi ) , *pPdf , specProp );
-        else
-            *pPdf = lerp( *pPdf , coat.pdf( swo , bsdfToBxdf(wi) ) , specProp );
+    if (!SameHemiSphere(wo, wi) || !PointingUp(wo)) {
+        if (pPdf) *pPdf = 0.0f;
+        return 0.0f;
     }
-    
-    return F(wo , wi);
+
+    return ret;
 }
 
 float Coat::Pdf( const Vector& wo , const Vector& wi ) const{
+    if (!SameHemiSphere(wo, wi)) return 0.0f;
+    if (!PointingUp(wo)) return 0.0f;
+
     Vector swo = bsdfToBxdf( wo );
     Vector swi = bsdfToBxdf( wi );
     
-    bool tir_o = false;
+    bool tir_o = false , tir_i = false;
     Vector r_wo = refract(swo, Vector(0.0f, 1.0f, 0.0f), ior, 1.0f, tir_o);
+    Vector r_wi = refract(swi, Vector(0.0f, 1.0f, 0.0f), ior, 1.0f, tir_i);
     const Spectrum attenuation = ( -thickness * sigma * 2.0f / AbsCosTheta(r_wo) ).Exp();
     const float I1 = fresnel.Evaluate(CosTheta(swo)).GetIntensity();
     const float I2 = ( 1.0f - I1 ) * ( 1.0f - I1 ) * attenuation.GetIntensity() / ( ior * ior );
     const float specProp = I1 / ( I1 + I2 );
 
     const float layer0_pdf = coat.pdf( swo , swi );
-    const float layer1_pdf = bottom->Pdf( wo , wi );
+    const float layer1_pdf = (tir_o || tir_i) ? 0.0f : bottom->Pdf(-r_wo, -r_wi);
     return lerp( layer1_pdf , layer0_pdf , specProp );
 }
