28797af81fb1a91068fb389e7d55a8c172af76ed
JiayinCao <caojiayin1985@gmail.com>
2018-12-21 17:41:31 +0800

    Fix compiling issue on Linux and Mac

--- src/math/bbox.h
+++ src/math/bbox.h
@@ -104,8 +104,8 @@ inline BBox Union( const BBox& bbox0 , const BBox& bbox1 )
 
 	for( int i = 0 ; i < 3 ; i++ )
 	{
-		result.m_Min[i] = std::fmin( bbox0.m_Min[i] , bbox1.m_Min[i] );
-		result.m_Max[i] = std::fmax( bbox0.m_Max[i] , bbox1.m_Max[i] );
+		result.m_Min[i] = std::min( bbox0.m_Min[i] , bbox1.m_Min[i] );
+		result.m_Max[i] = std::max( bbox0.m_Max[i] , bbox1.m_Max[i] );
 	}
 
 	return result;
@@ -141,8 +141,8 @@ inline float Intersect( const Ray& ray , const BBox& bb , float* fmax = 0 )
 				t2 = t;
 			}
 
-			tmin = std::fmax( t1 , tmin );
-			tmax = std::fmin( t2 , tmax );
+			tmin = std::max( t1 , tmin );
+			tmax = std::min( t2 , tmax );
 
 			if( tmin > tmax )
 				return -1.0f;
