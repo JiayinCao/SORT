a425bbae584f555dec51bda288cac5a8e8c20053
JerryCao1985 <caojiayin1985@icloud.com>
2018-12-27 21:05:49 +0800

    Implementation of memory stream

--- src/stream/mstream.h
+++ src/stream/mstream.h
@@ -18,21 +18,54 @@
 #pragma once
 
 #include "stream.h"
+#include "core/define.h"
+#include "stream/fstream.h"
 
 //! @brief Streaming from memory.
 /**
  * IMemoryStream only works for streaming data from memory. Any attempt to write data
  * to memory will result in immediate crash.
  */
-class IMemoryStream : public IStreamBase
-{
+class IMemoryStream : public IStreamBase{
 public:
+    //! @brief  Constructor.
+    //!
+    //! It is the caller's job to make sure the memory passed in has at least 'initSize'
+    //! bytes available. There will be unknown behavior otherwise.
+    //!
+    //! @param  initSize    Initial size of the stream.
+    //! @param  initData    Initial data of the stream.
+    IMemoryStream( unsigned int initSize = 1024 , void* initData = nullptr ){
+        Resize( initSize );
+        memcpy( m_data , initData , initSize );
+    }
+
+    //! @bried  Destructor.
+    ~IMemoryStream(){
+        SAFE_DELETE_ARRAY( m_data );
+    }
+
+    //! @brief  Resize the stream.
+    //!
+    //! @param  size    The new size to be resized.
+    inline void    Resize( unsigned int size ){
+        m_data = (char *) ( ( m_data == NULL ) ? malloc(size) : realloc(m_data, size) );
+        if (size > m_capacity)
+            memset((char*)m_data + m_capacity, 0, size - m_capacity);
+	    m_capacity = size;
+    }
+
     //! @brief Streaming in a float number to memory.
     //!
     //! @param v    Value to be loaded.
     //! @return     Reference of the stream itself.
     StreamBase& operator >> (float& v) override {
-        // to be implemented
+        if( m_pos + sizeof( v ) > m_capacity )
+            v = 0;
+        else{
+            memcpy( &v , m_data + m_pos , sizeof( v ) );
+            m_pos += sizeof( v );
+        }
         return *this;
     }
 
@@ -41,7 +74,12 @@ public:
     //! @param v    Value to be loaded.
     //! @return     Reference of the stream itself.
     StreamBase& operator >> (int& v) override {
-        // to be implemented
+        if( m_pos + sizeof( v ) > m_capacity )
+            v = 0;
+        else{
+            memcpy( &v , m_data + m_pos , sizeof( v ) );
+            m_pos += sizeof( v );
+        }
         return *this;
     }
 
@@ -50,7 +88,12 @@ public:
     //! @param v    Value to be loaded.
     //! @return     Reference of the stream itself.
     StreamBase& operator >> (unsigned int& v) override {
-        // to be implemented
+        if( m_pos + sizeof( v ) > m_capacity )
+            v = 0;
+        else{
+            memcpy( &v , m_data + m_pos , sizeof( v ) );
+            m_pos += sizeof( v );
+        }
         return *this;
     }
 
@@ -62,7 +105,13 @@ public:
     //! @param v    Value to be loaded.
     //! @return     Reference of the stream itself.
     StreamBase& operator >> (std::string& v) override {
-        // to be implemented
+        v = "";
+        while( m_pos < m_capacity ){
+            char c = m_data[m_pos++];
+            if( c == 0 )
+                break;
+            v += c;
+        }
         return *this;
     }
 
@@ -71,9 +120,31 @@ public:
     //! @param v    Value to be loaded.
     //! @return     Reference of the stream itself.
     StreamBase& operator >> (bool& v) override {
-        // to be implemented
+        if( m_pos + sizeof( v ) > m_capacity )
+            v = 0;
+        else{
+            memcpy( &v , m_data + m_pos , sizeof( v ) );
+            m_pos += sizeof( v );
+        }
         return *this;
     }
+
+    //! @brief  Get raw memory.
+    //!
+    //! The caller doesn't own the memory. It has to make sure its life time still expand its usage.
+    //!
+    //! @return     Return a pointer points to the memory.
+    void*       GetRawData(){
+        return m_data;
+    }
+
+private:
+    /**< Pointer points to the address where the memory is. */
+    char*               m_data = nullptr;
+    /**< Capacity of the current stream. */
+    unsigned int        m_capacity = 0;
+    /**< Current position of streaming. */
+    unsigned int        m_pos = 0;
 };
 
 //! @brief Streaming to memory.
@@ -81,15 +152,45 @@ public:
  * OMemoryStream only works for streaming data to memory. Any attempt to read data
  * from memory will result in immediate crash.
  */
-class OMemoryStream : public OStreamBase
-{
+class OMemoryStream : public OStreamBase{
 public:
+    //! @brief  Constructor.
+    //!
+    //! It is the caller's job to make sure the memory passed in has at least 'initSize'
+    //! bytes available. There will be unknown behavior otherwise.
+    //!
+    //! @param  initSize    Initial size of the stream.
+    OMemoryStream( unsigned int initSize = 1024 ){
+        Resize( initSize );
+    }
+
+    //! @bried  Destructor.
+    ~OMemoryStream(){
+        SAFE_DELETE_ARRAY( m_data );
+    }
+
+    //! @brief  Resize the stream.
+    //!
+    //! @param  size    The new size to be resized.
+    inline void    Resize( unsigned int size ){
+        m_data = (char *) ( ( m_data == NULL ) ? malloc(size) : realloc(m_data, size) );
+        if (size > m_capacity)
+            memset((char*)m_data + m_capacity, 0, size - m_capacity);
+	    m_capacity = size;
+    }
+
     //! @brief Streaming out a float number from memory.
     //!
     //! @param v    Value to be saved.
     //! @return     Reference of the stream itself.
     StreamBase& operator << (const float v) override {
-        // to be implemented
+        if( m_pos + sizeof( v ) > m_capacity ){
+            Resize( 2 * m_capacity );
+            return *this << v;
+        }else{
+            memcpy( m_data + m_pos , &v , sizeof( v ) );
+            m_pos += sizeof( v );
+        }
         return *this;
     }
 
@@ -98,7 +199,13 @@ public:
     //! @param v    Value to be saved.
     //! @return     Reference of the stream itself.
     StreamBase& operator << (const int v) override {
-        // to be implemented
+        if( m_pos + sizeof( v ) > m_capacity ){
+            Resize( 2 * m_capacity );
+            return *this << v;
+        }else{
+            memcpy( m_data + m_pos , &v , sizeof( v ) );
+            m_pos += sizeof( v );
+        }
         return *this;
     }
 
@@ -107,7 +214,13 @@ public:
     //! @param v    Value to be saved.
     //! @return     Reference of the stream itself.
     StreamBase& operator << (const unsigned int v) override {
-        // to be implemented
+        if( m_pos + sizeof( v ) > m_capacity ){
+            Resize( 2 * m_capacity );
+            return *this << v;
+        }else{
+            memcpy( m_data + m_pos , &v , sizeof( v ) );
+            m_pos += sizeof( v );
+        }
         return *this;
     }
 
@@ -119,7 +232,11 @@ public:
     //! @param v    Value to be saved.
     //! @return     Reference of the stream itself.
     StreamBase& operator << (const std::string& v) override {
-        // to be implemented
+        if( !v.empty() ){
+            memcpy( m_data + m_pos , v.c_str() , sizeof(char) * v.size() );
+            m_pos += v.size();
+        }
+        m_data[m_pos++] = 0;
         return *this;
     }
 
@@ -128,7 +245,37 @@ public:
     //! @param v    Value to be saved.
     //! @return     Reference of the stream itself.
     StreamBase& operator << (const bool v) override {
-        // to be implemented
+        if( m_pos + sizeof( v ) > m_capacity ){
+            Resize( 2 * m_capacity );
+            return *this << v;
+        }else{
+            memcpy( m_data + m_pos , &v , sizeof( v ) );
+            m_pos += sizeof( v );
+        }
         return *this;
     }
+
+    //! @brief Get raw memory.
+    //!
+    //! The caller doesn't own the memory. It has to make sure its life time still expand its usage.
+    //!
+    //! @return     Return a pointer points to the memory.
+    void*           GetRawData(){
+        return m_data;
+    }
+
+    //! @brief  Get written size of the stream.
+    //!
+    //! @return     The size of written bytes.
+    unsigned int    GetDataSize() const { 
+        return m_pos;
+    }
+
+private:
+    /**< Pointer points to the address where the memory is. */
+    char*               m_data = nullptr;
+    /**< Capacity of the current stream. */
+    unsigned int        m_capacity = 0;
+    /**< Current position of streaming. */
+    unsigned int        m_pos = 0;
 };
