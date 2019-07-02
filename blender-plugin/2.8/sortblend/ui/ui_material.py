66ccb7810403276a1e6fa38373f89cee2e65b993
JiayinCao <caojiayin1985@gmail.com>
2019-07-02 18:46:07 +0800

    Blender 2.8 support
    - Material socket panel added

--- blender-plugin/2.8/sortblend/ui/ui_material.py
+++ blender-plugin/2.8/sortblend/ui/ui_material.py
@@ -16,6 +16,7 @@
 import bpy
 from .. import base
 from ..material import nodes
+from ..material import group
 
 # Whether there is output node in the material node tree
 def has_sort_output_node(ntree):
@@ -101,3 +102,136 @@ class MATERIAL_PT_MaterialSlotPanel(SORTMaterialPanel, bpy.types.Panel):
         elif context.material:
             split.template_ID(context.space_data, "pin_id")
             split.separator()
+
+@base.register_class
+class OpNodeSocketMove(bpy.types.Operator):
+    """Move socket"""
+    bl_idname = "sort.node_socket_move"
+    bl_label = "Move Socket"
+
+    type : bpy.props.EnumProperty(
+        items=(('up', '', ''),
+               ('down', '', ''),
+               ('remove', '', ''),
+               ),
+    )
+    pos : bpy.props.IntProperty()
+    node_name : bpy.props.StringProperty()
+
+    def execute(self, context):
+        node = context.space_data.edit_tree.nodes[self.node_name]
+        tree = node.id_data
+        kind = node.node_kind
+        io = getattr(node, kind)
+        socket = io[self.pos]
+
+        if self.type == 'remove':
+            io.remove(socket)
+            # update instances
+            for instance in group.instances(tree):
+                sockets = getattr(instance, group.map_lookup[kind])
+                sockets.remove(sockets[self.pos])
+        else:
+            step = -1 if self.type == 'up' else 1
+            count = len(io) - 1
+
+            def calc_new_position(pos, step, count):
+                return max(0, min(pos + step, count - 1))
+
+            new_pos = calc_new_position(self.pos, step, count)
+            io.move(self.pos, new_pos)
+            # update instances
+            for instance in group.instances(tree):
+                sockets = getattr(instance, group.map_lookup[kind])
+                new_pos = calc_new_position(self.pos, step, len(sockets))
+                sockets.move(self.pos, new_pos)
+
+        group.update_cls(tree)
+        return {"FINISHED"}
+
+@base.register_class
+class OpNodeSocketRemove(OpNodeSocketMove):
+    """Remove socket"""
+    bl_idname = "sort.node_socket_remove"
+    bl_label = "Remove Socket"
+
+    type : bpy.props.EnumProperty(
+        items=(('up', '', ''),
+               ('down', '', ''),
+               ('remove', '', ''),
+               ),
+    )
+    pos : bpy.props.IntProperty()
+    node_name : bpy.props.StringProperty()
+
+@base.register_class
+class OpNodeSocketChangeName(bpy.types.Operator):
+    """Change socket name"""
+    bl_idname = "sort.node_socket_change_name"
+    bl_label = "Change Socket Name"
+
+    def execute(self, context):
+        return {"FINISHED"}
+
+@base.register_class
+class SORTInOutGroupEditor(SORTMaterialPanel, bpy.types.Panel):
+    bl_label = "SORT In/Out Group Editor"
+    bl_space_type = 'NODE_EDITOR'
+    bl_region_type = 'UI'
+
+    @classmethod
+    def poll(cls, context):
+        tree = context.space_data.edit_tree
+        if not tree:
+            return False
+        return tree.bl_idname == nodes.SORTShaderNodeTree.bl_idname and group.is_sort_node_group(tree)
+
+    def draw(self, context):
+        tree = context.space_data.edit_tree
+        input_node = tree.nodes.get("Group Inputs")
+        output_node = tree.nodes.get("Group Outputs")
+
+        if not (input_node and output_node):
+            return
+
+        layout = self.layout
+        row = layout.row()
+        if context.region.width > 340:
+            row = layout.row()
+            split = row.split(factor=0.5)
+            col1 = split.box().column()
+            split = split.split()
+            col2 = split.box().column()
+        else:
+            col1 = layout.row().box().column()
+            layout.separator()
+            col2 = layout.row().box().column()
+
+        def set_attrs(cls, **kwargs):
+            for name, value in kwargs.items():
+                setattr(cls, name, value)
+
+        def draw_socket(col, socket, index):
+            if socket.bl_idname == 'sort_dummy_socket':
+                return
+            params = dict(node_name=socket.node.name, pos=index)
+
+            row = col.row(align=True)
+            row.template_node_socket(color=(0.35, 0.5, 0.8, 1.0))
+            row.label(text=socket.name)
+
+            op = row.operator('sort.node_socket_move', icon='TRIA_UP', text='')
+            set_attrs(op, type='up', **params)
+            op = row.operator('sort.node_socket_move', icon='TRIA_DOWN', text='')
+            set_attrs(op, type='down', **params)
+            op = row.operator('sort.node_socket_remove', icon='X', text='')
+            set_attrs(op, type='remove', **params)
+            op = row.operator('sort.node_socket_change_name', icon='MODIFIER', text='')
+
+        col1.label(text='Inputs:')
+        for i, socket in enumerate(input_node.outputs):
+            draw_socket(col1, socket, i)
+
+        col2.label(text='Outputs:')
+        for i, socket in enumerate(output_node.inputs):
+            draw_socket(col2, socket, i)
