d2c4a66ff3c25a7fb5c48e462c43b317e2acc6e2
JiayinCao <caojiayin1985@gmail.com>
2019-07-20 08:25:05 +0800

    Fixed the error of groupping node with NodeFrame in it
    - Both of 2.7x and 2.8 versions are fixed.

--- blender-plugin/2.78/sortblend/material.py
+++ blender-plugin/2.78/sortblend/material.py
@@ -745,7 +745,7 @@ class SORT_Node_Group_Make_Operator(bpy.types.Operator):
             
         tree = context.space_data.edit_tree
         for node in tree.nodes:
-            if node.bl_idname == 'NodeReroute':
+            if node.bl_idname == 'NodeReroute' or node.bl_idname == 'NodeFrame':
                 continue
             if node.isGroupInputNode() or node.isGroupOutputNode() or node.isShaderGroupInputNode() or node.bl_idname == SORTNodeOutput.bl_idname:
                 node.select = False
