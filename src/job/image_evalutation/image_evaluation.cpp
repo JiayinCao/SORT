b20271ebcc0e5787e252d3b8240b66c8367bc4b4
JiayinCao <caojiayin1985@gmail.com>
2021-02-07 18:54:55 -0800

    [Marl] add some marl code in SORT

--- src/job/image_evalutation/image_evaluation.cpp
+++ src/job/image_evalutation/image_evaluation.cpp
@@ -16,9 +16,58 @@
  */
 
 #include "image_evaluation.h"
+#include "marl/defer.h"
+#include "marl/event.h"
+#include "marl/scheduler.h"
+#include "marl/waitgroup.h"
 
 void ImageEvaluation::StartRunning(int argc, char** argv, IStreamBase& stream) {
 
+    // this is really just to make sure it compiles and runs well on all platforms
+    // this will be deleted immediately after I verify it.
+    {
+        // Create a marl scheduler using the 4 hardware threads.
+        // Bind this scheduler to the main thread so we can call marl::schedule()
+        marl::Scheduler::Config cfg;
+        cfg.setWorkerThreadCount(4);
+
+        marl::Scheduler scheduler(cfg);
+        scheduler.bind();
+        defer(scheduler.unbind());  // Automatically unbind before returning.
+
+        constexpr int numTasks = 10;
+
+        // Create an event that is manually reset.
+        marl::Event sayHello(marl::Event::Mode::Manual);
+
+        // Create a WaitGroup with an initial count of numTasks.
+        marl::WaitGroup saidHello(numTasks);
+
+        // Schedule some tasks to run asynchronously.
+        for (int i = 0; i < numTasks; i++) {
+            // Each task will run on one of the 4 worker threads.
+            marl::schedule([=] {  // All marl primitives are capture-by-value.
+                // Decrement the WaitGroup counter when the task has finished.
+                defer(saidHello.done());
+
+                printf("Task %d waiting to say hello...\n", i);
+
+                // Blocking in a task?
+                // The scheduler will find something else for this thread to do.
+                sayHello.wait();
+
+                printf("Hello from task %d!\n", i);
+                });
+        }
+
+        sayHello.signal();  // Unblock all the tasks.
+
+        saidHello.wait();  // Wait for all tasks to complete.
+
+        printf("All tasks said hello.\n");
+
+        // All tasks are guaranteed to complete before the scheduler is destructed.
+    }
 }
 
 int ImageEvaluation::WaitForJobToBeDone() {
