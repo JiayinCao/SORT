365e03763947dbdbe64da1fe29b75ba2d3ccfdde
JiayinCao <caojiayin1985@gmail.com>
2021-02-09 13:14:13 -0800

    [Minor] Less wait before the end

--- src/sort.cpp
+++ src/sort.cpp
@@ -90,7 +90,7 @@ int RunSORT(int argc, char** argv) {
         SortStatsPrintData();
 
     // this sucks
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
     return ret;
 }
