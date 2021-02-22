457b75cba7d8ed5bf834b0ec38be26b249a5372c
Jiayin Cao <caojiayin1985@gmail.com>
2021-02-21 21:44:25 -0800

    [Minor] replace tabs with four spaces in bat file

--- make.bat
+++ make.bat
@@ -92,104 +92,104 @@ if NOT "%1" == "" (
 
 if "%SHOW_HELP%" == "1" (
     py .\scripts\show_help.py
-	goto EOF
+    goto EOF
 )
 
 if "%SHOW_ABOUT%" == "1" (
     py .\scripts\show_about.py
-	goto EOF
+    goto EOF
 )
 
 if "%SHOW_DEP_INFO%" == "1" (
     py .\scripts\show_dep_info.py
-	goto EOF
+    goto EOF
 )
 
 if "%GIT_UPDATE%" == "1" (
-	echo Syncing source code from Github
-	git pull
-	goto EOF
+    echo Syncing source code from Github
+    git pull
+    goto EOF
 )
 
 if "%UPDATE_DEP%" == "1" (
-	echo Syncing dependencies
-	py .\scripts\get_dep.py
-	goto EOF
+    echo Syncing dependencies
+    py .\scripts\get_dep.py
+    goto EOF
 )
 
 if "%FORCE_UPDATE_DEP%" == "1" (
-	echo Syncing dependencies
-	py .\scripts\get_dep.py TRUE
-	goto EOF
+    echo Syncing dependencies
+    py .\scripts\get_dep.py TRUE
+    goto EOF
 )
 
 if "%CLEAN_DEP%" == "1" (
-	echo Cleaning all dependency files
-	powershell Remove-Item -path ./dependencies -recurse -ErrorAction Ignore
-	goto EOF
+    echo Cleaning all dependency files
+    powershell Remove-Item -path ./dependencies -recurse -ErrorAction Ignore
+    goto EOF
 )
 
 if "%CLEAN%" == "1" (
-	echo Cleaning all generated file
-	powershell Remove-Item -path ./bin -recurse -ErrorAction Ignore
-	powershell Remove-Item -path ./_out -recurse -ErrorAction Ignore
-	powershell Remove-Item -path ./proj_release -recurse -ErrorAction Ignore
-	powershell Remove-Item -path ./proj_debug -recurse -ErrorAction Ignore
-	powershell Remove-Item -path ./generated_src -recurse -ErrorAction Ignore
-	goto EOF
+    echo Cleaning all generated file
+    powershell Remove-Item -path ./bin -recurse -ErrorAction Ignore
+    powershell Remove-Item -path ./_out -recurse -ErrorAction Ignore
+    powershell Remove-Item -path ./proj_release -recurse -ErrorAction Ignore
+    powershell Remove-Item -path ./proj_debug -recurse -ErrorAction Ignore
+    powershell Remove-Item -path ./generated_src -recurse -ErrorAction Ignore
+    goto EOF
 )
 
 if "%BUILD_RELEASE%" == "1" (
-	make update_dep
-	powershell New-Item -Force -ItemType directory -Path proj_release
-	cd proj_release
-	cmake -A x64 ..
-	msbuild /p:Configuration=Release SORT.sln
+    make update_dep
+    powershell New-Item -Force -ItemType directory -Path proj_release
+    cd proj_release
+    cmake -A x64 ..
+    msbuild /p:Configuration=Release SORT.sln
 
-	:: catch msbuild error
-	if ERRORLEVEL 1 ( 
-		goto BUILD_ERR
-	)
+    :: catch msbuild error
+    if ERRORLEVEL 1 ( 
+        goto BUILD_ERR
+    )
 
-	cd ..
+    cd ..
 )
 
 if "%BUILD_RELWITHDEBINFO%" == "1" (
-	make update_dep
-	powershell New-Item -Force -ItemType directory -Path proj_relwithdebinfo
-	cd proj_relwithdebinfo
-	cmake -A x64 ..
-	msbuild /p:Configuration=RelWithDebInfo SORT.sln
+    make update_dep
+    powershell New-Item -Force -ItemType directory -Path proj_relwithdebinfo
+    cd proj_relwithdebinfo
+    cmake -A x64 ..
+    msbuild /p:Configuration=RelWithDebInfo SORT.sln
 
-	:: catch msbuild error
-	if ERRORLEVEL 1 ( 
-		goto BUILD_ERR
-	)
+    :: catch msbuild error
+    if ERRORLEVEL 1 ( 
+        goto BUILD_ERR
+    )
 
-	cd ..
+    cd ..
 )
 
 if "%BUILD_DEBUG%" == "1" (
-	make update_dep
-	powershell New-Item -Force -ItemType directory -Path proj_debug
-	cd proj_debug
-	cmake -A x64 ..
-	msbuild /p:Configuration=Debug SORT.sln
+    make update_dep
+    powershell New-Item -Force -ItemType directory -Path proj_debug
+    cd proj_debug
+    cmake -A x64 ..
+    msbuild /p:Configuration=Debug SORT.sln
 
-	:: catch msbuild error
-	if ERRORLEVEL 1 ( 
-		goto BUILD_ERR
-	)
+    :: catch msbuild error
+    if ERRORLEVEL 1 ( 
+        goto BUILD_ERR
+    )
 
-	cd ..
+    cd ..
 )
 
 if "%REGISTER_SYS_ENV%" == "1" (
-	echo Register environment variables
-	echo Setting system environment 'SORT_ROOT_DIR' to %cd%
-	setx SORT_ROOT_DIR %cd%/ /M
-	echo Setting system environment 'SORT_BIN_DIR' to %cd%/bin
-	setx SORT_BIN_DIR %cd%/bin/ /M
+    echo Register environment variables
+    echo Setting system environment 'SORT_ROOT_DIR' to %cd%
+    setx SORT_ROOT_DIR %cd%/ /M
+    echo Setting system environment 'SORT_BIN_DIR' to %cd%/bin
+    setx SORT_BIN_DIR %cd%/bin/ /M
 )
 
 if "%GENERATE_SRC%" == "1" (
@@ -202,9 +202,9 @@ if "%VERIFY_BUILDS%" == "1" (
     py .\scripts\verify_builds.py
 
     :: catch msbuild error
-	if ERRORLEVEL 1 (
-		goto BUILD_ERR
-	)
+    if ERRORLEVEL 1 (
+        goto BUILD_ERR
+    )
 )
 
 :EOF
